6c0af89e124d7582a92c1095f5b33b2620617af9
szorfein <szorfein@protonmail.com>
2019-03-25 15:20:41 +1300

    hashes

---
+++ x03/hashes.rb
@@ -0,0 +1,42 @@
+# Creating new hashes with 3 methods
+# 1, the shorter
+h1 = {}
+# 2 with Hash.new
+h2 = Hash.new("Some kind of ring")
+# 3 with Hash[ ] class method, return an error if no paired
+h3 = Hash["room1", "The Treasure Room", "Room2", "The Throne Room"]
+puts h3
+
+# Insert new items
+h2['treasure1'] = 'Silver ring'
+# or
+h2.[]=("treasure2", "Gold ring")
+# or with store
+h2.store("treasure3", "Diamond ring")
+
+puts h2
+
+# shorter way
+h1 = { 'room1' => 'The treasure Room',
+       'room2' => 'The Throne Room',
+       'loc1' => 'A Forest Glade',
+       'loc2' => 'A Mountain Stream' }
+
+puts h1['room2']
+
+# Or too
+hash = { red: "ruby", white: "diamond", green: "emerald" }
+puts hash[:red]
+
+# Complex collections
+users_list = {
+  john: {
+    phone: "555-1234",
+    email: "john@example.com"
+  },
+  eric: {
+    phone: "555-1234",
+    email: "eric@example.com" }
+}
+# get an element with dig
+puts users_list.dig(:eric, :email) #=> eric@example.com
